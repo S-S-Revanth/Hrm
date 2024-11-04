8613483a9df96b3ee18ec915318152b1eb1e656c
S-S-Revanth <revanth52892@gmail.com>
2024-11-05 04:03:23 +0530

    initial commit

---
+++ src/Cmp/Dsh.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Grid} from '@mui/system'
+import { Link } from 'react-router-dom'
+function Finance() {
+  return (
+    <div>
+      <Grid container>  
+        <Grid size={2}>
+        <h1>HRM</h1>
+         <p> DashBoard</p>
+       <Link to='/finance'> <p>Finance</p></Link>   
+        </Grid>
+        <Grid size={10}>
+        <div className="card-row">
+          <h1> DashBoard</h1>
+        <div className="card">
+          <h2>Total Users</h2>
+          <p>15,234</p>
+        </div>
+        <div className="card">
+          <h2>New Orders</h2>
+          <p>382</p>
+        </div>
+        <div className="card">
+          <h2>Revenue</h2>
+          <p>$120,450</p>
+        </div>
+        </div>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default Finance
---
+++ src/Cmp/Finance.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Grid} from '@mui/system'
+import { Link } from 'react-router-dom'
+function Finance() {
+  return (
+    <div>
+      <Grid container>  
+        <Grid size={2}>
+        <h1>HRM</h1>
+        <Link to='/'> <p> DashBoard</p></Link> 
+          <p>Finance</p>
+        </Grid>
+        <Grid size={10}>
+        <div className="card-row">
+          <h1> Finance</h1>
+
+        <div className="card">
+          <h2>Total Users</h2>
+          <p>15,234</p>
+        </div>
+        <div className="card">
+          <h2>New Orders</h2>
+          <p>382</p>
+        </div>
+        <div className="card">
+          <h2>Revenue</h2>
+          <p>$120,450</p>
+        </div>
+        </div>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default Finance
